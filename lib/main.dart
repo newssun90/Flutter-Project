ff712a828a6453738a76dde05453db7acbeb8685
newssun90 <eheex01@gmail.com>
2023-10-26 20:29:35 +0900

    fetch data

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/main_simple.dart';
-import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
+import 'package:toonflix/webtoon/home_toonflix.dart';
 
 void main() {
-  runApp(const MaterialApp(title: 'Pro', home: App(),));
+  runApp(const MaterialApp(
+    title: 'Pro',
+    home: App(),
+  ));
+  ApiService().getTodaysToons();
 }
 
 class App extends StatelessWidget {
@@ -12,29 +17,32 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+            backgroundColor: const Color(0xFFE7626C),
           ),
+          textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Color(0xFF232B55),
+            ),
+          ),
+          cardColor: const Color(0xFFF4EDDB),
         ),
-        cardColor:const Color(0xFFF4EDDB),
-      ),
-      home: Scaffold(
-        body: const Center(
-          child: MyWidget(),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => const MySimpleWidget()));
-          }, 
-          child: const Icon(Icons.add_box_rounded),
-        ),
-        floatingActionButtonLocation:FloatingActionButtonLocation.endFloat,
-      )
-    );
+        home: Scaffold(
+          body: const Center(
+            //child: MyWidget(),
+            child: ToonFlixWidget(),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const MySimpleWidget()));
+            },
+            child: const Icon(Icons.add_box_rounded),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        ));
   }
 }
