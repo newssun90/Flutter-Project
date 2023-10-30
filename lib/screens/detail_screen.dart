8923fae2950ff7b5369bffd31b7cd05cd1515d1e
newssun90 <eheex01@gmail.com>
2023-10-30 19:29:04 +0900

    webtoon API

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class DetailScreenWidget extends StatelessWidget {
+class DetailScreenWidget extends StatefulWidget {
   final String title, thumb, id;
 
   const DetailScreenWidget({
@@ -11,13 +14,29 @@ class DetailScreenWidget extends StatelessWidget {
   });
 
   @override
+  State<DetailScreenWidget> createState() => _DetailScreenWidgetState();
+}
+
+class _DetailScreenWidgetState extends State<DetailScreenWidget> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    // initState는 build 전에 호출된다.
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.w500,
@@ -29,16 +48,16 @@ class DetailScreenWidget extends StatelessWidget {
       ),
       body: Column(children: [
         const SizedBox(
-          height: 50,
+          height: 25,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Hero(
               // Hero
-              tag: id,
+              tag: widget.id,
               child: Container(
-                width: 280,
+                width: 200,
                 clipBehavior: Clip.hardEdge,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15),
@@ -50,12 +69,47 @@ class DetailScreenWidget extends StatelessWidget {
                       )
                     ]),
                 child: Image.network(
-                  thumb,
+                  widget.thumb,
                 ),
               ),
             ),
           ],
         ),
+        const SizedBox(
+          height: 20,
+        ),
+        FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 40,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: const TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return const Text('...');
+            }),
       ]),
     );
   }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_detail_model.dart';
+import 'package:toonflix/models/webtoon_episode_model.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   static const String baseUrl =
       'http://webtoon-crawler.nomadcoders.workers.dev/';
   static const String today = 'today';
+  static const String episodes = 'episodes';
 
   static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
@@ -18,6 +21,31 @@ class ApiService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse('$baseUrl$id');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstance = [];
+    final url = Uri.parse('$baseUrl$id/$episodes');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstance.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstance;
     }
     throw Error();
   }
